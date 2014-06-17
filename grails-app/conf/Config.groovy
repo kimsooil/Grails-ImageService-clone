cd146954f90882b11eda3f582b3cfce6bc752a24
Eric Pierce <epierce@usf.edu>
2014-06-17 12:53:58 -0400

    Change web app context

--- grails-app/conf/Config.groovy
+++ grails-app/conf/Config.groovy
@@ -47,6 +47,9 @@ grails.exceptionresolver.params.exclude = ['password']
 // configure auto-caching of queries by default (if false you can cache individual queries with 'cache: true')
 grails.hibernate.cache.queries = false
 
+// Set the URL to / instead of /ImageService
+grails.app.context = '/'
+
 environments {
     development {
         grails.logging.jul.usebridge = true
