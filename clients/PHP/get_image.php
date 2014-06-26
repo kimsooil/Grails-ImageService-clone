81323e5f9bd6913af3c8334555615424a6e248e0
Eric Pierce <epierce@usf.edu>
2014-06-26 12:46:04 -0400

    Add more config options to PHP client

--- clients/PHP/get_image.php
+++ clients/PHP/get_image.php
@@ -1,14 +1,16 @@
 <?php
         include('ImageServiceClient.php');
 
+        $imageServiceScheme = 'http';
         $imageServiceHost = 'localhost';
         $imageServicePort = 8080;
+        $imageServicePath = '/ImageService';
 
         //Get the standard resolution (200x200) image
-        echo ImageServiceClient::getImageURL($imageServiceHost, $imageServicePort ,$argv[1], 'test', 'AfoaKlDM4AjVyjo38f0NOs4O6hXM1T32'). "\n";
+        echo ImageServiceClient::getImageURL($imageServiceScheme, $imageServiceHost, $imageServicePort, $imageServicePath, $argv[1], 'test', 'AfoaKlDM4AjVyjo38f0NOs4O6hXM1T32'). "\n";
 
 
         //Get a specific size image
         $width = 400;
         $height = 400;
-        echo ImageServiceClient::getResizedImageURL($imageServiceHost, $imageServicePort, $argv[1], 'test', 'AfoaKlDM4AjVyjo38f0NOs4O6hXM1T32', $width, $height). "\n";
+        echo ImageServiceClient::getResizedImageURL($imageServiceScheme, $imageServiceHost, $imageServicePort, $imageServicePath, $argv[1], 'test', 'AfoaKlDM4AjVyjo38f0NOs4O6hXM1T32', $width, $height). "\n";
