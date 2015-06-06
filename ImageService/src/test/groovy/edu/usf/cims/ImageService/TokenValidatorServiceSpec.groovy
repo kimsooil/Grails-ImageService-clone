15e50ac150ee6fdfd540606c8767dc22d3b9ce8b
Eric Pierce <epierce@usf.edu>
2015-06-06 14:11:15 -0400

    Add tests for SHA1 and Hmac hashed tokens

--- ImageService/src/test/groovy/edu/usf/cims/ImageService/TokenValidatorServiceSpec.groovy
+++ ImageService/src/test/groovy/edu/usf/cims/ImageService/TokenValidatorServiceSpec.groovy
@@ -21,6 +21,20 @@ class TokenValidatorServiceSpec extends Specification {
                 encoding: 'ASCII',
                 key: 'AfoaKlDM4AjVyjo38f0NOs4O6hXM1T32'
         ]
+        c.image_service.services.sha1_test = [
+                tokenAlg: 'SHA-1',
+                privacy: false,
+                separator: '_',
+                encoding: 'ASCII',
+                key: '8fKqPyfAah56cRXM0Qafkom10zn7Upw2'
+        ]
+        c.image_service.services.shaHmac_test = [
+                tokenAlg: 'HmacSHA1',
+                privacy: false,
+                separator: '_',
+                encoding: 'ASCII',
+                key: 'bfTkmK8CxKAQMABJ5Wg1xmqycvvxkdLa'
+        ]
     }
 
     def setup() {
@@ -55,7 +69,7 @@ class TokenValidatorServiceSpec extends Specification {
         result == [result:"success", message:"U12345678"]
     }
 
-    def "Bad token - expired"() {
+    def "Bad AES token - expired"() {
         given:
         def tokenValidatorService=grailsApplication.mainContext.getBean('tokenValidatorService')
         def token = Security.AESencrypt("00000000000|U12345678", config.image_service.services.aes_test.key)
@@ -68,7 +82,7 @@ class TokenValidatorServiceSpec extends Specification {
         result.message.contains('out of range')
     }
 
-    def "Bad token - future dated"() {
+    def "Bad AES token - future dated"() {
         given:
         def tokenValidatorService=grailsApplication.mainContext.getBean('tokenValidatorService')
         def token = Security.AESencrypt("999999999999|U12345678", config.image_service.services.aes_test.key)
@@ -81,7 +95,7 @@ class TokenValidatorServiceSpec extends Specification {
         result.message.contains('out of range')
     }
 
-    def "Bad key value"() {
+    def "Bad AES key value"() {
         given:
         def tokenValidatorService=grailsApplication.mainContext.getBean('tokenValidatorService')
         def unixTime = new Date().getTime() / 1000 as Integer
@@ -95,7 +109,7 @@ class TokenValidatorServiceSpec extends Specification {
     }
 
 
-    def "Bad token format"() {
+    def "Bad AES token format"() {
         given:
         def tokenValidatorService=grailsApplication.mainContext.getBean('tokenValidatorService')
         def unixTime = new Date().getTime() / 1000 as Integer
@@ -107,5 +121,57 @@ class TokenValidatorServiceSpec extends Specification {
         then:
         result.result == "error"
         result.message.contains('Token decryption failed or incorrect token format')
+    }
+
+    def "Good SHA-1 hashed token"() {
+        given:
+        def tokenValidatorService=grailsApplication.mainContext.getBean('tokenValidatorService')
+        def tokenText = String.format('%1$s%2$s', 'U12345678', config.image_service.services.sha1_test.key)
+        def token = "U12345678_${Security.digest(tokenText, 'SHA-1', 'ASCII')}"
+
+        when:
+        def result = tokenValidatorService.validateToken('sha1_test', token)
+
+        then:
+        result == [result:"success", message:"U12345678"]
+    }
+
+    def "Bad SHA-1 hash (wrong key)"() {
+        given:
+        def tokenValidatorService=grailsApplication.mainContext.getBean('tokenValidatorService')
+        def tokenText = String.format('%1$s%2$s', 'U12345678', '12345678901234567890123456789012')
+        def token = "U12345678_${Security.digest(tokenText, 'SHA-1', 'ASCII')}"
+
+        when:
+        def result = tokenValidatorService.validateToken('sha1_test', token)
+
+        then:
+        result.result == "error"
+        result.message.contains('not valid for U12345678')
+    }
+
+    def "Good HmacSHA1 hashed token"() {
+        given:
+        def tokenValidatorService=grailsApplication.mainContext.getBean('tokenValidatorService')
+        def token = "U12345678_${Security.HMACdigest('U12345678', config.image_service.services.shaHmac_test.key, 'HmacSHA1', 'ASCII')}"
+
+        when:
+        def result = tokenValidatorService.validateToken('shaHmac_test', token)
+
+        then:
+        result == [result:"success", message:"U12345678"]
+    }
+
+    def "Bad HmacSHA1 hash (wrong key)"() {
+        given:
+        def tokenValidatorService=grailsApplication.mainContext.getBean('tokenValidatorService')
+        def token = "U12345678_${Security.HMACdigest('U12345678', '12345678901234567890123456789012', 'HmacSHA1', 'ASCII')}"
+
+        when:
+        def result = tokenValidatorService.validateToken('sha1_test', token)
+
+        then:
+        result.result == "error"
+        result.message.contains('not valid for U12345678')
     }
 }
