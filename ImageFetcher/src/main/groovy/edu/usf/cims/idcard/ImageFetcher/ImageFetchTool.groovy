44a38772edc17e77fce250b673d07429b01ca659
James Jones <jamjon3@gmail.com>
2017-02-22 13:53:15 -0500

    Another variation

--- ImageFetcher/src/main/groovy/edu/usf/cims/idcard/ImageFetcher/ImageFetchTool.groovy
+++ ImageFetcher/src/main/groovy/edu/usf/cims/idcard/ImageFetcher/ImageFetchTool.groovy
@@ -46,7 +46,7 @@ class ImageFetchTool {
                   System.out.println(JsonOutput.toJson(idsql.firstRow("""SELECT * FROM IDCARD.ID WHERE ROWNUM <= 1""")))
                   // return """SELECT '${o.usfid.value as String}' AS usfid FROM DUAL"""
                   def usfid = o.usfid.value as String
-                  return """SELECT ID.ID_PERSON AS USFID FROM IDCARD.ID WHERE ID.ID_IMAGE_FILE_NAME IS NOT NULL AND ID.ID_PERSON LIKE '""" + usfid + """' GROUP BY ID.ID_PERSON"""
+                  return ("""SELECT ID.ID_PERSON AS USFID FROM IDCARD.ID WHERE ID.ID_IMAGE_FILE_NAME IS NOT NULL AND ID.ID_PERSON LIKE '""" + usfid + """' GROUP BY ID.ID_PERSON""").toString()
                 } else {
                   def date = new Date().format('yyyyMMdd') as String
                   if (o.date) {
