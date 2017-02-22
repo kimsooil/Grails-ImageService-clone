910b8845ac8954590e21ec3ccd952a17d5915696
James Jones <jamjon3@gmail.com>
2017-02-21 22:53:00 -0500

    moving alias quotes back

--- ImageFetcher/src/main/groovy/edu/usf/cims/idcard/ImageFetcher/ImageFetchTool.groovy
+++ ImageFetcher/src/main/groovy/edu/usf/cims/idcard/ImageFetcher/ImageFetchTool.groovy
@@ -45,7 +45,7 @@ class ImageFetchTool {
                 } else if(o.usfid) {
                   // return """SELECT ID_PERSON AS "USFID" FROM IDCARD.ID WHERE ID_IMAGE_FILE_NAME IS NOT NULL AND ID_PERSON LIKE ${o.usfid.value} GROUP BY ID_PERSON"""
                   System.out.println(JsonOutput.toJson(idsql.firstRow("""SELECT * FROM IDCARD.ID WHERE ROWNUM <= 1""")))
-                  return """SELECT '${o.usfid.value as String}' AS USFID FROM DUAL"""
+                  return """SELECT '${o.usfid.value as String}' AS "USFID" FROM DUAL"""
                 } else {
                   def date = new Date().format('yyyyMMdd') as String
                   if (o.date) {
