115ba1c0659727d035ee4e7b85dd851b434431c8
James Jones <jamjon3@gmail.com>
2017-02-21 20:35:31 -0500

    added a catch block

--- ImageFetcher/src/main/groovy/edu/usf/cims/idcard/ImageFetcher/ImageFetchTool.groovy
+++ ImageFetcher/src/main/groovy/edu/usf/cims/idcard/ImageFetcher/ImageFetchTool.groovy
@@ -38,97 +38,103 @@ class ImageFetchTool {
             def inactiveCardListSQL = "SELECT ID_PERSON, ID_IMAGE_FILE_NAME, ID_ISSUE_DATE FROM IDCARD.ID WHERE ID_IMAGE_FILE_NAME IS NOT NULL AND ID_PERSON LIKE :usfid ORDER BY ID_ISSUE_DATE DESC"
             def activeCardSQL = "SELECT ID_PERSON, ID_IMAGE_FILE_NAME FROM IDCARD.ID WHERE ID_IMAGE_FILE_NAME IS NOT NULL AND ID_ACTIVE_CODE='A' AND ID_PERSON LIKE :usfid AND ROWNUM = 1 ORDER BY ID_ISSUE_DATE DESC"            
             // Get a list of all persons who appear to have a picture
-            idsql.eachRow({ o ->
-              if (o.all) {
-                return "SELECT ID_PERSON AS USFID FROM IDCARD.ID WHERE ID_IMAGE_FILE_NAME IS NOT NULL GROUP BY ID_PERSON" as String
-              } else if(o.usfid) {
-                return "SELECT ID_PERSON AS USFID FROM IDCARD.ID WHERE ID_IMAGE_FILE_NAME IS NOT NULL AND ID_PERSON LIKE '${o.usfid.value}' GROUP BY ID_PERSON" as String
-              } else {
-                def date = new Date().format('yyyyMMdd') as String
-                if (o.date) {
-                  date = o.date.value
-                }
-                return "SELECT ID_PERSON AS USFID FROM IDCARD.ID WHERE ID_IMAGE_FILE_NAME IS NOT NULL AND ID_ISSUE_DATE > TO_DATE('${date}','YYYYMMDD') GROUP BY ID_PERSON" as String
-              }
-              return 
-            }.call(opt)) { urow ->
-                // Check to see if the user is active 
-              System.out.println(JsonOutput.toJson(urow))
-              def oldimages = []
-              def transferimage = { ipath,newFileLocation ->
-                def patharr = ipath.trim().tokenize('\\')
-                def fileName = patharr.pop()
-                while(patharr.size() > 0) {
-                  def i = new File(new File([config.origBaseDir,patharr.join('/')].join('/')),fileName)
-                  log.debug "Trying to read ${i.absolutePath} => ${newFileLocation}"
-                  if(i.canRead()) {
-                    BufferedImage imageData = ImageIO.read(i)
-                    
-                    log.debug "${i.absolutePath} => ${newFileLocation}"
-                    BufferedImage thumbnail = Scalr.resize(imageData, Scalr.Method.SPEED, Scalr.Mode.FIT_TO_HEIGHT, 200, 200)
-                    def cropX = thumbnail.getWidth() / 2 as int
-                    
-                    // Add a white matte around the image so that we can crop it square and not lose any of the image
-                    thumbnail = Scalr.pad(thumbnail, 100, java.awt.Color.WHITE)
-                    thumbnail = Scalr.crop(thumbnail,cropX,100,200,200)
-                    ImageIO.write(thumbnail, 'JPEG', new File(newFileLocation))
-                    thumbnail.flush()
-                    log.debug "Transferring ${i.path} to ${newFileLocation}"
-                    return true;
-                  } else {
-                    patharr.remove(0)
-                  }
-                }
-                return false
-              }
-              if(idsql.firstRow(activeCardCheckSQL.toString(),[usfid:urow.USFID]).FOUND) {
-                def ac = idsql.firstRow(activeCardSQL.toString(),[usfid:urow.USFID])
-                if(namssql.firstRow(privacyCheckSQL.toString(),[usfid:urow.USFID]).found) {
-                  oldimages.plus([new File("${config.inactiveDir}/${urow.USFID}.jpg"),new File("${config.newBaseDir}/${urow.USFID}.jpg")])
-                  oldimages.each{ i -> 
-                    if(i.canRead()) {
-                      boolean fileMoved = i.renameTo(new File(new File(config.privateDir), i.getName()))
-                    }                  
-                  }
-                  oldimages.clear()
-                  if(transferimage.call(ac.ID_IMAGE_FILE_NAME,"${config.privateDir}/${urow.USFID}.jpg")) {
-                    images++
-                    toPrivate++
-                  }
+            try {
+              idsql.eachRow({ o ->
+                if (o.all) {
+                  return "SELECT ID_PERSON AS USFID FROM IDCARD.ID WHERE ID_IMAGE_FILE_NAME IS NOT NULL GROUP BY ID_PERSON" as String
+                } else if(o.usfid) {
+                  return "SELECT ID_PERSON AS USFID FROM IDCARD.ID WHERE ID_IMAGE_FILE_NAME IS NOT NULL AND ID_PERSON LIKE '${o.usfid.value}' GROUP BY ID_PERSON" as String
                 } else {
-                  oldimages.plus([new File("${config.inactiveDir}/${urow.USFID}.jpg"),new File("${config.privateDir}/${urow.USFID}.jpg")])
-                  oldimages.each{ i -> 
+                  def date = new Date().format('yyyyMMdd') as String
+                  if (o.date) {
+                    date = o.date.value
+                  }
+                  return "SELECT ID_PERSON AS USFID FROM IDCARD.ID WHERE ID_IMAGE_FILE_NAME IS NOT NULL AND ID_ISSUE_DATE > TO_DATE('${date}','YYYYMMDD') GROUP BY ID_PERSON" as String
+                }
+                return 
+              }.call(opt)) { urow ->
+                  // Check to see if the user is active 
+                System.out.println(JsonOutput.toJson(urow))
+                def oldimages = []
+                def transferimage = { ipath,newFileLocation ->
+                  def patharr = ipath.trim().tokenize('\\')
+                  def fileName = patharr.pop()
+                  while(patharr.size() > 0) {
+                    def i = new File(new File([config.origBaseDir,patharr.join('/')].join('/')),fileName)
+                    log.debug "Trying to read ${i.absolutePath} => ${newFileLocation}"
                     if(i.canRead()) {
-                      boolean fileMoved = i.renameTo(new File(new File(config.newBaseDir), i.getName()))
-                    }                  
+                      BufferedImage imageData = ImageIO.read(i)
+
+                      log.debug "${i.absolutePath} => ${newFileLocation}"
+                      BufferedImage thumbnail = Scalr.resize(imageData, Scalr.Method.SPEED, Scalr.Mode.FIT_TO_HEIGHT, 200, 200)
+                      def cropX = thumbnail.getWidth() / 2 as int
+
+                      // Add a white matte around the image so that we can crop it square and not lose any of the image
+                      thumbnail = Scalr.pad(thumbnail, 100, java.awt.Color.WHITE)
+                      thumbnail = Scalr.crop(thumbnail,cropX,100,200,200)
+                      ImageIO.write(thumbnail, 'JPEG', new File(newFileLocation))
+                      thumbnail.flush()
+                      log.debug "Transferring ${i.path} to ${newFileLocation}"
+                      return true;
+                    } else {
+                      patharr.remove(0)
+                    }
+                  }
+                  return false
+                }
+                if(idsql.firstRow(activeCardCheckSQL.toString(),[usfid:urow.USFID]).FOUND) {
+                  def ac = idsql.firstRow(activeCardSQL.toString(),[usfid:urow.USFID])
+                  if(namssql.firstRow(privacyCheckSQL.toString(),[usfid:urow.USFID]).found) {
+                    oldimages.plus([new File("${config.inactiveDir}/${urow.USFID}.jpg"),new File("${config.newBaseDir}/${urow.USFID}.jpg")])
+                    oldimages.each{ i -> 
+                      if(i.canRead()) {
+                        boolean fileMoved = i.renameTo(new File(new File(config.privateDir), i.getName()))
+                      }                  
+                    }
+                    oldimages.clear()
+                    if(transferimage.call(ac.ID_IMAGE_FILE_NAME,"${config.privateDir}/${urow.USFID}.jpg")) {
+                      images++
+                      toPrivate++
+                    }
+                  } else {
+                    oldimages.plus([new File("${config.inactiveDir}/${urow.USFID}.jpg"),new File("${config.privateDir}/${urow.USFID}.jpg")])
+                    oldimages.each{ i -> 
+                      if(i.canRead()) {
+                        boolean fileMoved = i.renameTo(new File(new File(config.newBaseDir), i.getName()))
+                      }                  
+                    }
+                    oldimages.clear()
+                    if(transferimage.call(ac.ID_IMAGE_FILE_NAME,"${config.newBaseDir}/${urow.USFID}.jpg")) {
+                      images++
+                      toPublic++
+                    }
+                  }                             
+                } else {
+                  // Move any images from private or public to inactive
+                  oldimages.plus([new File("${config.newBaseDir}/${urow.USFID}.jpg"),new File("${config.privateDir}/${urow.USFID}.jpg")])
+                  oldimages.each{ i ->
+                    if(i.canRead()) {
+                      boolean fileMoved = i.renameTo(new File(new File(config.inactiveDir), i.getName()))
+                    }
                   }
                   oldimages.clear()
-                  if(transferimage.call(ac.ID_IMAGE_FILE_NAME,"${config.newBaseDir}/${urow.USFID}.jpg")) {
-                    images++
-                    toPublic++
-                  }
-                }                             
-              } else {
-                // Move any images from private or public to inactive
-                oldimages.plus([new File("${config.newBaseDir}/${urow.USFID}.jpg"),new File("${config.privateDir}/${urow.USFID}.jpg")])
-                oldimages.each{ i ->
-                  if(i.canRead()) {
-                    boolean fileMoved = i.renameTo(new File(new File(config.inactiveDir), i.getName()))
-                  }
-                }
-                oldimages.clear()
-                boolean found = false
-                idsql.eachRow(inactiveCardListSQL.toString(),[usfid:urow.USFID]) { ia ->
-                  if(!found) {
-                    found = transferimage.call(ia.ID_IMAGE_FILE_NAME,"${config.inactiveDir}/${urow.USFID}.jpg")
-                    if(found) { 
-                      images++ 
-                      toInactive++
+                  boolean found = false
+                  idsql.eachRow(inactiveCardListSQL.toString(),[usfid:urow.USFID]) { ia ->
+                    if(!found) {
+                      found = transferimage.call(ia.ID_IMAGE_FILE_NAME,"${config.inactiveDir}/${urow.USFID}.jpg")
+                      if(found) { 
+                        images++ 
+                        toInactive++
+                      }
                     }
                   }
                 }
               }
+            } catch(Exception e) {
+              exitOnError e.message
             }
+              
+            
           }
           def now = System.currentTimeMillis()
           log.info "${now-start}ms|images: ${images}|toPublic: ${toPublic}|toPrivate: ${toPrivate}|toInactive: ${toInactive}"
