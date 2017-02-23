555ac83ef2d8c0a6da564b8dbd669fc5b22b0a65
James Jones <jamjon3@gmail.com>
2017-02-23 13:13:12 -0500

    Added much needed comments now that this logic is broken up

--- ImageFetcher/src/main/groovy/edu/usf/cims/idcard/ImageFetcher/ImageFetchTool.groovy
+++ ImageFetcher/src/main/groovy/edu/usf/cims/idcard/ImageFetcher/ImageFetchTool.groovy
@@ -13,13 +13,24 @@ import org.imgscalr.Scalr
 import org.imgscalr.Scalr.*
 
 /**
-* ImageFetchTool
-**/
+ * ImageFetchTool
+ * 
+ * This tool reads from a file share and does a structured copy as used by ImageService.
+ * Much of this logic had to be rewritten in 2017 to handle inactive cards and
+ * do 'best case' matching against an ID card directory structure that is out of sync with 
+ * what's recorded in it's database.
+ * 
+ * @author Eric Pierce
+ * @author James Jones
+ * @company University of South Florida
+ */
 @Slf4j
 class ImageFetchTool {
 
   public static String PATTERN = "%d{ABSOLUTE} %-5p [%c{1}] %m%n"
-
+  /**
+   * Handles the list of cards that will be processed by this tool 
+   */
   public static void main(String[] args) {
     def start = System.currentTimeMillis()
     def opt = getCommandLineOptions(args)
@@ -81,6 +92,15 @@ class ImageFetchTool {
       e.printStackTrace(System.out);
     }    
   }
+  /**
+   * Handles the transfer of the image off the file share to the correct folder for ImageService
+   * 
+   * @param     ipath               The path recorded in the ID card system
+   * @param     newFileLocation     The destination of the image for ImageService
+   * @param     config              The config objects derrived via the commandline
+   * @return    The boolean success or failure fo the image transfer
+   * @see boolean
+   */ 
   private static transferimage(ipath,newFileLocation,config) {
     def patharr = ipath.trim().tokenize('\\')
     def fileName = patharr.pop()
@@ -115,7 +135,16 @@ class ImageFetchTool {
     }
     return false
   }
-  
+  /**
+   * Aligns Id Card image (when found) into a private, public or inactive folder
+   * 
+   * @param     urow     An object containing the target USFID
+   * @param     idsql    Database connection for ID Card
+   * @param     namssql  Database connection for NAMS
+   * @param     config   The config objects derrived via the commandline
+   * @return    A map object containing the summary of the process results
+   * @see Map
+   */
   private static processCard(urow,idsql,namssql,config) {
     def activeCardCheckSQL = "SELECT COUNT(*) AS FOUND FROM IDCARD.ID WHERE ID_IMAGE_FILE_NAME IS NOT NULL AND ID_ACTIVE_CODE='A' AND ID_PERSON LIKE :usfid AND ROWNUM = 1"
     def inactiveCardListSQL = "SELECT ID_PERSON, ID_IMAGE_FILE_NAME, ID_ISSUE_DATE FROM IDCARD.ID WHERE ID_IMAGE_FILE_NAME IS NOT NULL AND ID_PERSON LIKE :usfid ORDER BY ID_ISSUE_DATE DESC"
@@ -127,7 +156,6 @@ class ImageFetchTool {
       toInactive: 0,
       images: 0
     ]
-    // System.out.println(JsonOutput.toJson(urow))
     def oldimages = []
     if(idsql.firstRow(activeCardCheckSQL.toString(),[usfid:urow.USFID]).FOUND) {
       def ac = idsql.firstRow(activeCardSQL.toString(),[usfid:urow.USFID])
@@ -178,7 +206,13 @@ class ImageFetchTool {
     }
     return summary
   }
-  
+  /**
+   * Handles incoming command line options and returns the parsed object
+   * 
+   * @param  args  An array of argements originating from the options passed on the command line
+   * @return The parsed command object from the CliBuilder
+   * @see Map
+   */
   private static getCommandLineOptions(String[] args){
     //Parse command-line options
     def cli = new CliBuilder(
@@ -204,7 +238,14 @@ class ImageFetchTool {
 
     return options
   }
-
+  /**
+   * Merge the config file that was passed on the commandline with the options 
+   * from the command line
+   * 
+   * @param  options
+   * @return A config slurper object containing the merged settings
+   * @see ConfigObject
+   */
   private static getConfigSettings(options){
     def config = new ConfigObject()
 
