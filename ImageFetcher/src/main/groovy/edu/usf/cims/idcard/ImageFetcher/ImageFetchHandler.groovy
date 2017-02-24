ed8864454c3c3ab8daeabe06476b9af49b100998
James Jones <jamjon3@gmail.com>
2017-02-23 21:27:20 -0500

    removed uneeded looping

--- ImageFetcher/src/main/groovy/edu/usf/cims/idcard/ImageFetcher/ImageFetchHandler.groovy
+++ ImageFetcher/src/main/groovy/edu/usf/cims/idcard/ImageFetcher/ImageFetchHandler.groovy
@@ -43,18 +43,14 @@ class ImageFetchHandler {
     if(this.opt.all) {
       def getusfidsSQL = "SELECT ID_PERSON AS USFID FROM IDCARD.ID WHERE ID_IMAGE_FILE_NAME IS NOT NULL GROUP BY ID_PERSON"
       this.idsql.eachRow(getusfidsSQL.toString()) { r ->
-        processId(r.USFID).each({ k,v ->
-          this.summary[k] += v
-        })
+        this.processId(r.USFID)
       }
     } else if(this.opt.usfid) {
       def usfid = this.opt.usfid.value as String
       System.out.println(usfid)
       def getusfidsSQL = "SELECT ID.ID_PERSON AS USFID FROM IDCARD.ID WHERE ID.ID_IMAGE_FILE_NAME IS NOT NULL AND ID.ID_PERSON LIKE ? GROUP BY ID.ID_PERSON"
       this.idsql.eachRow(getusfidsSQL.toString(),[ usfid ]) { r ->
-        processId(r.USFID).each({ k,v ->
-          this.summary[k] += v
-        })
+        this.processId(r.USFID)
       }
     } else {
       def date = new Date().format('yyyyMMdd') as String
@@ -63,9 +59,7 @@ class ImageFetchHandler {
       }
       def getusfidsSQL = "SELECT ID_PERSON AS USFID FROM IDCARD.ID WHERE ID_IMAGE_FILE_NAME IS NOT NULL AND ID_ISSUE_DATE > TO_DATE( ? ,'YYYYMMDD') GROUP BY ID_PERSON"
       this.idsql.eachRow(getusfidsSQL.toString(), [ date ]) { r-> 
-        processId(r.USFID).each({ k,v ->
-          this.summary[k] += v
-        })
+        this.processId(r.USFID)
       }
     }
     def now = System.currentTimeMillis()
